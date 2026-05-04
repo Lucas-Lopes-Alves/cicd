a1b9f5eb98ba43f8b632f31a1abb824393084577
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-05-04 08:27:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('Erro ao carregar o arquivo HTML');
+            return;
+        }
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(content);
+    });
+});
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
